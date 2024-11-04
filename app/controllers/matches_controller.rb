406825b2d07ab0e3b483ca86394211a2e9e6d68d
Jesher Minelli <jesherdevsk8@gmail.com>
2024-11-03 23:09:14 -0300

    improve layout and add flash messages

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,10 +1,11 @@
 class MatchesController < ApplicationController
   before_action :set_match, only: %i[ show edit update destroy ]
-  before_action :match_results, only: %i[ new create ]
+  before_action :match_results, only: %i[ new create edit ]
+  # before_action :selected_players, only: :create
 
   # GET /matches or /matches.json
   def index
-    @matches = Match.all
+    @pagy, @matches = pagy(Match.order(date: :desc))
   end
 
   # GET /matches/1 or /matches/1.json
@@ -26,11 +27,22 @@ class MatchesController < ApplicationController
   def create
     @match = Match.new(match_params)
 
+    if selected_players.size < 12
+      respond_to do |format|
+        flash[:error] = 'Não é permitido criar uma partida com menos de 12 jogadores.'
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: { error: 'Não é permitido criar uma partida com menos de 12 jogadores.' }, status: :unprocessable_entity }
+      end
+      return
+    end
+
     respond_to do |format|
       if @match.save
-        format.html { redirect_to @match, notice: 'Match was successfully created.' }
+        flash[:notice] = 'Partida criada com sucesso!'
+        format.html { redirect_to @match }
         format.json { render :show, status: :created, location: @match }
       else
+        flash[:error] = 'Houve um erro ao criar a partida.'
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @match.errors, status: :unprocessable_entity }
       end
@@ -41,9 +53,11 @@ class MatchesController < ApplicationController
   def update
     respond_to do |format|
       if @match.update(match_params)
-        format.html { redirect_to @match, notice: 'Match was successfully updated.' }
+        flash[:notice] = 'Partida atualizada com sucesso!'
+        format.html { redirect_to @match }
         format.json { render :show, status: :ok, location: @match }
       else
+        flash[:error] = 'Houve um erro ao atualizar a partida!'
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @match.errors, status: :unprocessable_entity }
       end
@@ -55,7 +69,8 @@ class MatchesController < ApplicationController
     @match.destroy!
 
     respond_to do |format|
-      format.html { redirect_to matches_path, status: :see_other, notice: 'Match was successfully destroyed.' }
+      flash[:notice] = 'Partida deletada com sucesso!'
+      format.html { redirect_to matches_path, status: :see_other }
       format.json { head :no_content }
     end
   end
@@ -64,12 +79,22 @@ class MatchesController < ApplicationController
 
   def match_results
     @match_results ||= Participation.match_results.except('_prefix')
-                                    .invert.map {|k, v| [ k.humanize, v]}
+                                    .invert.map { |k, v| [ k.humanize, v ] }
   end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_match
     @match = Match.find(params[:id])
+  end
+
+  def selected_players
+    permitted_params = match_params.to_h[:participations_attributes]&.with_indifferent_access
+    return [] if permitted_params.blank?
+
+    filtered_players ||= permitted_params.select { |_, attributes| attributes['_destroy'] == '0' }
+    player_ids = filtered_players.map { |player| player.last[:player_id] }
+
+    @selected_players ||= Player.where(id: player_ids)
   end
 
   # Only allow a list of trusted parameters through.
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  include Pagy::Frontend
+
+  def flash_message
+    messages = ''
+    %i[notice info warning error].each do |type|
+      messages += "<div class=\"base-alert #{type}\" role='alert'>#{flash[type]}</div>" if flash[type]
+    end
+    messages.html_safe
+  end
 end
