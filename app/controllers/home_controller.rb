67b8417196baea3acb24cbd3292a745b23391fe7
Jesher Minelli <jesherdevsk8@gmail.com>
2024-11-04 00:29:26 -0300

    improve layout and add some data counters

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,11 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @total_matches = Match.count
+    @total_players = Player.count
+    @total_goals   = Participation.sum(:goals)
+    # flash[:notice] = 'Login efetuado com sucesso!'
+    # flash[:info] = 'Você tem mensagens não visualizadas'
+    # flash[:warning] = 'País deve ser selecionado'
+    # flash[:error] = 'Não foi possível encontrar o imóvel'
+  end
 end
