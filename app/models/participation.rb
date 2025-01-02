4e4247716e958427a48e472329f5b414cfbeaa0a
Jesher Minelli <82419736+jesherdevsk8@users.noreply.github.com>
2025-01-02 15:24:40 -0300

    Feature/statistics by year (#11)
    * remove file dockerfile2
    
    * remove file dockerfile2 (#8) (#9)
    
    * feat: add min score and max score to home index
    
    * chore: rubocop linter
    
    * feat: add statistics by year

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -10,6 +10,10 @@ class Participation < ApplicationRecord
   validates :goals, numericality: { greater_than_or_equal_to: 0 }
   validates :match_result, :player, :match, :team, presence: true
 
+  scope :by_year, ->(year) {
+    where(created_at: Time.new(year).beginning_of_year..Time.new(year).end_of_year) if year.present?
+  }
+
   def self.results
     match_results.except('_prefix').invert.map { |k, v| [ k.humanize, v ] }
   end
