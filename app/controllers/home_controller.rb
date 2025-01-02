f55de95ff68ea90c4e4c0faa42e2473f09236e7f
Jesher Minelli <jesherdevsk8@gmail.com>
2025-01-02 20:30:00 -0300

    feat: add filter to home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  before_action :set_years, only: :index
+
   def index
     # TODO: Usar cache com Redis
     year = params[:year].present? ? Date.new(params[:year].to_i).year : Time.current.year
@@ -13,5 +15,12 @@ class HomeController < ApplicationController
     @min_score     = load_matches.by_year(year).min_score
     # TODO: Fazer calculo de empates, adicionar uma coluna resultado tbm na partida
     # @total_draws   = load_participations.by_year(year).draw.size
+  end
+
+  private
+
+  def set_years
+    @years ||= load_matches.select('DISTINCT EXTRACT(YEAR FROM date) AS year')
+                           .map { |match| match.year.to_i }
   end
 end
