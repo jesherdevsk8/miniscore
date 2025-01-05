6c2c39bed4a73e11995566c4d1cfb3a18d80fddf
Jesher Minelli <jesherdevsk8@gmail.com>
2025-01-05 18:34:40 -0300

    remove duplicate methods

--- app/models/team.rb
+++ app/models/team.rb
@@ -46,28 +46,4 @@ class Team < ApplicationRecord
       { name: player[0], goals: player[1] }
     end
   end
-
-  def get_players(current_year = Time.current.year)
-    players.participations_by_year(current_year).map do |player|
-      {
-        name: player.name,
-        statistics: {
-          total_goals: player.total_goals(current_year),
-          total_matches: player.total_matches(current_year),
-          victories: player.vitorias(current_year),
-          defeats: player.derrotas(current_year),
-          draws: player.empates(current_year),
-          average_goals_per_match: player.goals_per_matches(current_year),
-          performance_percentage: player.percentage(current_year),
-          last_five_participations: player.last_five_participations(current_year)
-        }
-      }
-    end
-  end
-
-  def get_top_scorers(current_year = Time.current.year)
-    players.participations_by_year(current_year).top_scorers(current_year).map do |player|
-      { name: player[0], goals: player[1] }
-    end
-  end
 end
