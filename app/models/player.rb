5d6b308eb2c834a6d37083c0b3237ad059a1eaad
Jesher Minelli <jesherdevsk8@gmail.com>
2025-01-12 20:06:37 -0300

    feat: add flag to ignore a non goalkeeper mode

--- app/models/player.rb
+++ app/models/player.rb
@@ -61,7 +61,7 @@ class Player < ApplicationRecord
   def average_goals_conceded_per_match(year = nil)
     return unless goalkeeper?
 
-    total_matches = total_matches(year)
+    total_matches = participations.by_year(year).where.not(non_goalkeeper_mode: true).size
     return 0 if total_matches.zero?
     total_match_goals = goals_conceded[year.to_s]
 
